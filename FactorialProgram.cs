27c6e8d8c715a192dbed9c5ead596e317db59e0f
shindeAvinash96 <127420673+shindeAvinash96@users.noreply.github.com>
2025-06-18 16:27:37 +0530

    Add files via upload

---
+++ FactorialProgram.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace FactorialProgram{
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+
+			Console.Write("Enter The Number: ");
+			int n= Convert.ToInt32(Console.ReadLine());
+			int factorial=1;
+
+			if(n<=0)
+			{
+				Console.Write("Enter +ve integer greater than 0.");
+			}
+
+			for(int i =1;i<=n;i++)
+			{
+				factorial*=i;
+			}
+			Console.Write("The factorial of number {0} is : "+ factorial);
+			
+		}
+	}
+}
