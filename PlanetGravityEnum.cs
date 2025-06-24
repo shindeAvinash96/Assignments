6bee0bfae977b5533e49583fb60d3f48e52498f7
shindeAvinash96 <127420673+shindeAvinash96@users.noreply.github.com>
2025-06-24 23:26:31 +0530

    Add files via upload

---
+++ PlanetGravityEnum.cs
@@ -0,0 +1,30 @@
+﻿namespace PlanetGravityEnum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int Earthweight = 70;
+
+            Console.WriteLine("Weight on Mars:  "+equivalentWeight(Earthweight,3.71));
+            Console.WriteLine("Weight on Jupiter:  " + equivalentWeight(Earthweight, 24.79));
+
+        }
+
+        static double equivalentWeight(double Earthweight, double planetGravity)
+        {
+            double EarthGravity = 9.8;
+            return Earthweight * (planetGravity / EarthGravity);
+        }
+    }
+
+    public enum Planet 
+    {
+        Earth,
+        Mars,
+        Jupiter
+    }
+
+
+}
