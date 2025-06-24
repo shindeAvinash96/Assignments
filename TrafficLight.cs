2a094c21fee92b24eb82bd5dfcb5f591c71dad7d
shindeAvinash96 <127420673+shindeAvinash96@users.noreply.github.com>
2025-06-24 20:37:38 +0530

    Add files via upload

---
+++ TrafficLight.cs
@@ -0,0 +1,46 @@
+﻿namespace TrafficSignalEnum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Signal(TrafficLight.Red));
+            Console.WriteLine(Signal(TrafficLight.Green));
+            Console.WriteLine(Signal(TrafficLight.Yellow));
+
+        }
+
+        static string Signal(TrafficLight Light)
+        {
+            switch (Light)
+            {
+                case TrafficLight.Red:
+                    return "Red->Stop";
+                 
+
+                case TrafficLight.Green:
+                    return "Green->Go";
+
+                case TrafficLight.Yellow:
+                    return "Yellow->Get ready";
+
+                default:
+
+                    return "Invalid data detected.";
+
+
+            }
+    
+        }
+    }
+
+    public enum TrafficLight
+    {
+        Red,
+        Yellow,
+        Green,
+
+    }
+
+    
+}
