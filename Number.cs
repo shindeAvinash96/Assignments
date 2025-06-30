e808e4aed232bc71a4f3a33b835752ef7e358914
shindeAvinash96 <127420673+shindeAvinash96@users.noreply.github.com>
2025-06-30 23:59:42 +0530

    Add files via upload

---
+++ Number.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigDiceWithCalisthenics
+{
+    public class Number
+    {
+        private readonly int value;
+
+        public Number(int value)
+        {
+            this.value = value;
+        }
+
+        public bool IsOne()
+        {
+            return value == 1;
+        }
+
+        public int ToInt()
+        {
+            return value;
+        }
+
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+    }
+}
