1ddb6c6514a12b59f159b04e8c4a3cc66c3482b1
shindeAvinash96 <127420673+shindeAvinash96@users.noreply.github.com>
2025-07-16 12:51:53 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,16 +1,100 @@
-﻿namespace StudentGradingNew
+﻿// Abstract Products
+public interface IButton
 {
-    internal class Program
+    void Display();
+}
+
+public interface ICheckbox
+{
+    void Display();
+}
+
+// Concrete Products (Windows)
+public class WindowsButton :IButton
+{
+    public void Display()
     {
-        static void Main(string[] args)
+        Console.WriteLine("Rendering a Windows Button");
+    }
+}
+
+public class WindowsCheckbox : ICheckbox
+{
+    public void Display()
+    {
+        Console.WriteLine("Rendering a Windows Checkbox");
+    }
+}
+
+//Concrete Products(Mac)
+public class MacButton : IButton
+{
+    public void Display()
+    {
+        Console.WriteLine("Rendering a Mac Button");
+    }
+}
+
+public class MacCheckbox : ICheckbox
+{
+    public void Display()
+    {
+        Console.WriteLine("Rendering a Mac Checkbox");
+    }
+}
+
+// Abstract Factory
+public interface IGUIFactory
+{
+    IButton CreateButton();
+    ICheckbox CreateCheckbox();
+}
+
+// Concrete Factories
+public class WindowsFactory : IGUIFactory
+{
+    public IButton CreateButton() => new WindowsButton();
+    public ICheckbox CreateCheckbox() => new WindowsCheckbox();
+}
+
+public class MacFactory : IGUIFactory
+{
+    public IButton CreateButton() => new MacButton();
+    public ICheckbox CreateCheckbox() => new MacCheckbox();
+}
+
+// Client Code
+class Program
+{
+    static void RenderUI(IGUIFactory factory)
+    {
+        IButton button = factory.CreateButton();
+        ICheckbox checkbox = factory.CreateCheckbox();
+        button.Display();
+        checkbox.Display();
+    }
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("You want to create object of windows or Mac");
+        string choice = Console.ReadLine().ToLower();
+        
+        if (choice == "windows")
         {
-            int[] marks = new int[] { 78, 85, 90, 88, 76 };
-
-           
-            Student student = new Student("Avinash Shinde", "R12", marks);
-
-            
-            Console.WriteLine(student.ToString());
+            IGUIFactory factory = new WindowsFactory();
+            RenderUI(factory);
         }
+        else if(choice =="mac")
+        {
+
+            IGUIFactory factory1 = new MacFactory();
+            RenderUI(factory1);
+        }
+        else
+        {
+            Console.WriteLine("Invalid Choice");
+        }
+
     }
 }
